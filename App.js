759657a94cd8c7e634d8df21689fed7c08cc6b4e
Yasumatsu Noboru <yasumatsu_noboru@cyberagent.co.jp>
2018-02-17 16:07:07 +0900

    greeting banana

--- App.js
+++ App.js
@@ -8,19 +8,28 @@ class Greeting extends Component {
         );
     }
 }
-
-
-export default class LotsOfGreetings  extends Component {
+class Banana extends Component {
     render() {
         let pic = {
             uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
         };
+        return (
+            <Image source={pic} style={{width: 200, height: 200}}/>
+        );
+    }
+}
+
+
+export default class LotsOfGreetings  extends Component {
+    render() {
         return(
             <View style={{alignItems: 'center'}}>
                 <Greeting name='Rexxar' />
+                <Banana />
                 <Greeting name='Jania' />
+                <Banana />
                 <Greeting name='Valeera' />
-            <Image source={pic} style={{width: 200, height: 200}}/>
+                <Banana />
             </View>
         );
     }
