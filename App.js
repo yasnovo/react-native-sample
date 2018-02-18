372e88553ff0471f0e8c28bd3aec3edb1d1fd970
yasnovo <yasnovo@gmail.com>
2018-02-18 13:42:30 +0900

    tile full display

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default class FlexDimensionsBasics extends Component {
       // Try removing the `flex: 1` on the parent View.
       // The parent will not have dimentions, so the chldren can't expand.
       // what if you add `height: 300` instead of `flex: 1`
-      <View>
+      <View style={{flex: 1}}>
         <View style={{flex: 1, backgroundColor: 'powderblue'}} />
         <View style={{flex: 2, backgroundColor: 'skyblue'}} />
         <View style={{flex: 3, backgroundColor: 'steelblue'}} />
