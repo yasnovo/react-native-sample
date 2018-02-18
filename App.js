aac7a7ef015aee7199af8b61557417a7f0f692ef
yasnovo <yasnovo@gmail.com>
2018-02-18 19:10:20 +0900

    using FlatList

--- App.js
+++ App.js
@@ -1,40 +1,37 @@
 import React, { Component } from 'react';
-import { AppRegistry, ScrollView, Image, Text } from 'react-native';
+import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default class IScrollDownAndWhatHappendNextShockedMe extends Component {
+export default class FlatListBasics extends Component {
   render () {
-    let pic = {
-  uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-
     return (
-      <ScrollView>
-        <Text style={{fontSize: 96}}>Scroll me Plz</Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Text style={{fontSize: 96}}>Scroll me Plz</Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Text style={{fontSize: 96}}>Scroll me Plz</Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Text style={{fontSize: 96}}>Scroll me Plz</Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-
-      </ScrollView>
+      <View style={styles.container}>
+        <FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Devin'},
+            {key: 'Devin'},
+            {key: 'Devin'},
+            {key: 'Devin'},
+            {key: 'Devin'},
+            {key: 'Devin'},
+          ]}
+          renderItem={
+            ({item}) => <Text style={styles.item}>{item.key}</Text>
+          }
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
