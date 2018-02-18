8dcbd1e034361192ea0f41257772d9fd4072b283
yasnovo <yasnovo@gmail.com>
2018-02-18 13:04:24 +0900

    error

--- App.js
+++ App.js
@@ -1,36 +1,25 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isShowingText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.isShowingText ? this.props.text : ' ';
-    return (
-      <Text>{display}</Text>
-    );
-  }
-}
-
-export default class BlinkApp extends Component {
+export default class LotsOfStyles extends Component {
   render() {
     return (
       <View>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue then red</Text>
+        <Text style={[styles.red styles.bigblue]}>red, then bigblue</Text>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  bigblue:{
+    color: 'blue',
+    frontWeight: 'bold',
+    fontSize: 30,
+  },
+  red:{
+    color: 'red',
+  },
+});
