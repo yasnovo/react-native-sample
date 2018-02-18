868019e640eb405a68e1507cd3e900d8fac5d279
yasnovo <yasnovo@gmail.com>
2018-02-18 18:47:45 +0900

    scroll view

--- App.js
+++ App.js
@@ -1,71 +1,40 @@
 import React, { Component } from 'react';
-import { Alert, AppRegistry, Platform, StyleSheet, Text, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
+import { AppRegistry, ScrollView, Image, Text } from 'react-native';
 
-export default class Touchables extends Component {
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
-  }
-
-  _onLongPressButton() {
-    Alert.alert('You long-pressed the button!')
-  }
-
+export default class IScrollDownAndWhatHappendNextShockedMe extends Component {
   render () {
+    let pic = {
+  uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+
     return (
-      <View style={styles.container}>
-        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableHighlight</Text>
-          </View>
-        </TouchableHighlight>
+      <ScrollView>
+        <Text style={{fontSize: 96}}>Scroll me Plz</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Text style={{fontSize: 96}}>Scroll me Plz</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Text style={{fontSize: 96}}>Scroll me Plz</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Text style={{fontSize: 96}}>Scroll me Plz</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Image source={pic} style={{width: 193, height: 110}}/>
 
-        <TouchableOpacity onPress={this._onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableOpacity</Text>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableNativeFeedback
-          onPress={this._onPressButton}
-          background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
-          </View>
-        </TouchableNativeFeedback>
-
-        <TouchableWithoutFeedback
-          onPress={this._onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
-          </View>
-        </TouchableWithoutFeedback>
-
-        <TouchableHighlight
-          onPress={this._onPressButton}
-          onLongPress={this._onLongPressButton}
-          underlayColor="white">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Touchable with Long press</Text>
-          </View>
-        </TouchableHighlight>
-      </View>
+      </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 60,
-    alignItems: 'center'
-  },
-  button: {
-    marginBottom: 30,
-    width: 260,
-    alignItems: 'center',
-    backgroundColor: '#2196F3'
-  },
-  buttonText: {
-    padding: 20,
-    color: 'white'
-  }
-})
