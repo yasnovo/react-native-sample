5037a7a635b73cb5c3b0588943ca2fa72622cf3d
yasnovo <yasnovo@gmail.com>
2018-02-18 13:15:22 +0900

    fix

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
+
 export default class LotsOfStyles extends Component {
   render() {
     return (
       <View>
         <Text style={styles.red}>just red</Text>
         <Text style={styles.bigblue}>just bigblue</Text>
-        <Text style={[styles.bigblue, styles.red]}>bigblue then red</Text>
-        <Text style={[styles.red styles.bigblue]}>red, then bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
-  bigblue:{
+  bigblue: {
     color: 'blue',
-    frontWeight: 'bold',
+    fontWeight: 'bold',
     fontSize: 30,
   },
-  red:{
+  red: {
     color: 'red',
   },
 });
