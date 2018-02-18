bdd699d2d39560047ee6cc3bc25011f281805725
yasnovo <yasnovo@gmail.com>
2018-02-18 14:22:28 +0900

    flexDirection

--- App.js
+++ App.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
 
-export default class FlexDimensionsBasics extends Component {
+export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      // Try removing the `flex: 1` on the parent View.
-      // The parent will not have dimentions, so the chldren can't expand.
-      // what if you add `height: 300` instead of `flex: 1`
-      <View style={{height: 300}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
