5a6548c381772055e06f7adf402f56a9de5fde5f
yasnovo <yasnovo@gmail.com>
2018-02-18 13:41:29 +0900

    not shown

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 
 
-export default class FixedDimensionsBasics extends Component {
+export default class FlexDimensionsBasics extends Component {
   render() {
     return (
+      // Try removing the `flex: 1` on the parent View.
+      // The parent will not have dimentions, so the chldren can't expand.
+      // what if you add `height: 300` instead of `flex: 1`
       <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
